75c1dfd6c2ddb6f98bd3c62cc0f40b84f53f9688
dikos1030 <dikos1030@gmail.com>
2025-11-09 19:57:49 -0500

    Finished Week 1!

---
+++ Source/GAM312_DIKOS/GAM312PjctGameModeBase.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "GAM312PjctGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class GAM312_DIKOS_API AGAM312PjctGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
